9e8fc8afb6b63a041f3a216620f7996099331b03
Daniel Orr <orrdaniel73@gmail.com>
2022-01-09 00:52:21 +0000

    parrots as parakeets built-in resource pack

--- src/main/java/me/ninni/macaws/client/MacawsClient.java
+++ src/main/java/me/ninni/macaws/client/MacawsClient.java
@@ -9,12 +9,18 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import static me.ninni.macaws.Macaws.*;
+
 @Environment(EnvType.CLIENT)
 public class MacawsClient implements ClientModInitializer {
-    public static final Logger LOGGER = LogManager.getLogger("%s-client".formatted(Macaws.MOD_ID));
+    public static final Logger LOGGER = LogManager.getLogger("%s-client".formatted(MOD_ID));
 
     @SuppressWarnings("UnstableApiUsage")
     @Override
@@ -23,6 +29,11 @@ public class MacawsClient implements ClientModInitializer {
 
         Reflection.initialize(MacawEntityModelLayers.class);
         EntityRendererRegistry.register(MacawsEntities.MACAW, MacawEntityRenderer::new);
+
+        // register parrot reskin resource pack
+        FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(container -> {
+            ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(MOD_ID, "parakeets"), container, ResourcePackActivationType.DEFAULT_ENABLED);
+        });
 
         LOGGER.info("Initialized {}-client", Macaws.MOD_NAME);
     }
