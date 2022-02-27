fe9a1d25319df6e99d13660b48dc0160ddd61d6b
andantet <orrdaniel73@gmail.com>
2022-02-27 14:38:48 +0000

    convert loot table to datagen

--- src/main/java/net/moddingplayground/macaws/impl/data/MacawsToymakerImpl.java
+++ src/main/java/net/moddingplayground/macaws/impl/data/MacawsToymakerImpl.java
@@ -1,7 +1,9 @@
 package net.moddingplayground.macaws.impl.data;
 
+import net.minecraft.loot.context.LootContextTypes;
 import net.moddingplayground.frame.api.toymaker.v0.ToymakerEntrypoint;
 import net.moddingplayground.frame.api.toymaker.v0.registry.generator.ItemModelGeneratorStore;
+import net.moddingplayground.frame.api.toymaker.v0.registry.generator.LootGeneratorStore;
 import net.moddingplayground.frame.api.toymaker.v0.registry.generator.TagGeneratorStore;
 import net.moddingplayground.macaws.api.Macaws;
 
@@ -10,5 +12,6 @@ public class MacawsToymakerImpl implements Macaws, ToymakerEntrypoint {
     public void onInitializeToymaker() {
         TagGeneratorStore.register(() -> new BiomeTagGenerator(MOD_ID));
         ItemModelGeneratorStore.register(() -> new ItemModelGenerator(MOD_ID));
+        LootGeneratorStore.register(() -> new EntityTypeLootGenerator(MOD_ID), LootContextTypes.ENTITY);
     }
 }
