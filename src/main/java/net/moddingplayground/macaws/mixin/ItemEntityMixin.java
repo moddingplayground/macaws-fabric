e6007c3beae9c27d72eaccd97ebfd649b4ea3d4e
andantet <orrdaniel73@gmail.com>
2022-03-06 00:32:42 +0000

    fix novel item pickup detection

--- src/main/java/net/moddingplayground/macaws/mixin/ItemEntityMixin.java
+++ src/main/java/net/moddingplayground/macaws/mixin/ItemEntityMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.World;
 import net.moddingplayground.macaws.impl.entity.HeadMountAccess;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,6 +18,8 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin extends Entity {
+    private @Unique ItemStack macaws_preInsertStack = ItemStack.EMPTY;
+
     private ItemEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
@@ -25,15 +28,27 @@ public abstract class ItemEntityMixin extends Entity {
         method = "onPlayerCollision",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/entity/player/PlayerInventory;insertStack(Lnet/minecraft/item/ItemStack;)Z",
-            shift = At.Shift.AFTER
+            target = "Lnet/minecraft/item/ItemStack;getCount()I"
         ),
         locals = LocalCapture.CAPTURE_FAILHARD
     )
-    private void onOnPlayerCollisionAsInsertStack(PlayerEntity player, CallbackInfo ci, ItemStack stack) {
+    private void beforeOnPlayerCollisionInsertStack(PlayerEntity player, CallbackInfo ci, ItemStack stack) {
         PlayerInventory inventory = player.getInventory();
-        if (!inventory.contains(stack)) {
-            HeadMountAccess access = (HeadMountAccess) inventory.player;
+        this.macaws_preInsertStack = inventory.contains(stack) ? ItemStack.EMPTY : stack.copy();
+    }
+
+    @Inject(
+        method = "onPlayerCollision",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/player/PlayerEntity;sendPickup(Lnet/minecraft/entity/Entity;I)V",
+            shift = At.Shift.AFTER
+        )
+    )
+    private void afterOnPlayerCollisionInsertStack(PlayerEntity player, CallbackInfo ci) {
+        ItemStack stack = this.macaws_preInsertStack;
+        if (!stack.isEmpty()) {
+            HeadMountAccess access = (HeadMountAccess) player;
             NbtCompound nbt = access.getHeadEntity();
             if (!nbt.isEmpty()) access.onNovelItemPickUp(stack);
         }
